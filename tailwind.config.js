9126f04b4dff748d01c208d268ee4fcb77f8d31b
Tom den Boon <tomdenboon@hotmail.com>
2021-11-01 19:31:30 +0100

    altered animation added box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,15 +23,11 @@ module.exports = {
         },
         'fade-in-down': {
           '0%': {
-            transform: 'translateY(-10px)'
+            transform: 'translateY(-20px)'
           },
           '1%': {
             opacity: '0.05',
-            transform: 'translateY(-10px)'
-          },
-          '30%': {
-            opacity: '0.05',
-            transform: 'translateY(-10px)'
+            transform: 'translateY(-20px)'
           },
           '100%': {
             opacity: '1',
@@ -40,15 +36,11 @@ module.exports = {
         },
         'fade-in-up': {
           '0%': {
-            transform: 'translateY(10px)'
+            transform: 'translateY(20px)'
           },
           '1%': {
             opacity: '0.05',
-            transform: 'translateY(10px)'
-          },
-          '30%': {
-            opacity: '0.05',
-            transform: 'translateY(10px)'
+            transform: 'translateY(20px)'
           },
           '100%': {
             opacity: '1',
@@ -57,15 +49,11 @@ module.exports = {
         },
         'fade-in-left': {
           '0%': {
-            transform: 'translateX(-10px)'
+            transform: 'translateX(-20px)'
           },
           '1%': {
             opacity: '0.05',
-            transform: 'translateX(-10px)'
-          },
-          '30%': {
-            opacity: '0.05',
-            transform: 'translateX(-10px)'
+            transform: 'translateX(-20px)'
           },
           '100%': {
             opacity: '1',
@@ -74,15 +62,11 @@ module.exports = {
         }, 
         'fade-in-right': {
           '0%': {
-            transform: 'translateX(10px)'
+            transform: 'translateX(20px)'
           },
           '1%': {
             opacity: '0.05',
-            transform: 'translateX(10px)'
-          },
-          '30%': {
-            opacity: '0.05',
-            transform: 'translateX(10px)'
+            transform: 'translateX(20px)'
           },
           '100%': {
             opacity: '1',
@@ -91,27 +75,32 @@ module.exports = {
         }, 
       },
       animation: {
-        'fade-in': 'fade-in 0.4s ease-out both',
-        'fade-in-delay-800': 'fade-in 0.4s ease-out 0.8s both',
-        'fade-in-down-delay-100': 'fade-in-down 0.4s ease-out 0.1s both',
-        'fade-in-down-delay-200': 'fade-in-down 0.4s ease-out 0.2s both',
-        'fade-in-down-delay-300': 'fade-in-down 0.4s ease-out 0.3s both',
-        'fade-in-down-delay-400': 'fade-in-down 0.4s ease-out 0.4s both',
-        'fade-in-down-delay-500': 'fade-in-down 0.4s ease-out 0.5s both',
-        'fade-in-down-delay-800': 'fade-in-down 0.4s ease-out 0.8s both',
-        'fade-in-up-delay-100': 'fade-in-up 0.4s ease-out 0.1s both',
-        'fade-in-up-delay-200': 'fade-in-up 0.4s ease-out 0.2s both',
-        'fade-in-up-delay-300': 'fade-in-up 0.4s ease-out 0.3s both',
-        'fade-in-up-delay-600': 'fade-in-up 0.4s ease-out 0.6s both',
-        'fade-in-up-delay-700': 'fade-in-up 0.4s ease-out 0.7s both',
-        'fade-in-up-delay-800': 'fade-in-up 0.4s ease-out 0.8s both',
-        'fade-in-up-delay-900': 'fade-in-up 0.4s ease-out 0.9s both',
-        'fade-in-up-delay-1000': 'fade-in-up 0.4s ease-out 1.0s both',
-        'fade-in-up-delay-1100': 'fade-in-up 0.4s ease-out 1.1s both',
-        'fade-in-left-delay-600': 'fade-in-left 0.4s ease-out 0.6s both',
-        'fade-in-right-delay-700': 'fade-in-right 0.4s ease-out 0.7s both',
-        'fade-in-right-delay-800': 'fade-in-right 0.4s ease-out 0.8s both',
-        'fade-in-right-delay-900': 'fade-in-right 0.4s ease-out 0.9s both',
+        'fade-in': 'fade-in 0.3s ease-in both',
+        'fade-in-delay-800': 'fade-in 0.3s ease-in 0.8s both',
+        'fade-in-down-delay-100': 'fade-in-down 0.3s ease-in 0.1s both',
+        'fade-in-down-delay-200': 'fade-in-down 0.3s ease-in 0.2s both',
+        'fade-in-down-delay-300': 'fade-in-down 0.3s ease-out 0.3s both',
+        'fade-in-down-delay-400': 'fade-in-down 0.3s ease-in 0.4s both',
+        'fade-in-down-delay-500': 'fade-in-down 0.3s ease-in 0.5s both',
+        'fade-in-down-delay-600': 'fade-in-down 0.3s ease-in 0.6s both',
+        'fade-in-down-delay-700': 'fade-in-down 0.3s ease-in 0.7s both',
+        'fade-in-down-delay-800': 'fade-in-down 0.3s ease-in 0.8s both',
+        'fade-in-up-delay-100': 'fade-in-up 0.3s ease-in 0.1s both',
+        'fade-in-up-delay-200': 'fade-in-up 0.3s ease-in 0.2s both',
+        'fade-in-up-delay-300': 'fade-in-up 0.3s ease-in 0.3s both',
+        'fade-in-up-delay-600': 'fade-in-up 0.3s ease-in 0.6s both',
+        'fade-in-up-delay-700': 'fade-in-up 0.3s ease-in 0.7s both',
+        'fade-in-up-delay-800': 'fade-in-up 0.3s ease-in 0.8s both',
+        'fade-in-up-delay-900': 'fade-in-up 0.3s ease-in 0.9s both',
+        'fade-in-up-delay-1000': 'fade-in-up 0.3s ease-in 1.0s both',
+        'fade-in-up-delay-1100': 'fade-in-up 0.3s ease-in 1.1s both',
+        'fade-in-left-delay-600': 'fade-in-left 0.3s ease-in 0.6s both',
+        'fade-in-right-delay-200': 'fade-in-right 0.3s ease-in 0.2s both',
+        'fade-in-right-delay-300': 'fade-in-right 0.3s ease-in 0.3s both',
+        'fade-in-right-delay-700': 'fade-in-right 0.3s ease-in 0.7s both',
+        'fade-in-right-delay-800': 'fade-in-right 0.3s ease-in 0.8s both',
+        'fade-in-right-delay-900': 'fade-in-right 0.3s ease-in 0.9s both',
+
       }
     },
   },
